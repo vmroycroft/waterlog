ed0d8201bc89bdeb8f51547ff22fc6c473782fd1
Mary R <vmroycroft@gmail.com>
2020-05-30 10:50:33 -0400

    WIP React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BreakpointProvider, setDefaultBreakpoints } from 'react-socks';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './features/Home';
 
-function App() {
+// setDefaultBreakpoints([
+//   { xsmall: 0 },
+//   { small: 640 },
+//   { medium: 768 },
+//   { large: 1024 },
+//   { xlarge: 1280 }
+// ]);
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BreakpointProvider>
+      <Router>
+        <Switch as="main">
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </BreakpointProvider>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        tan: '#F6F2DF',
+        'dark-tan': '#e6e3d1',
+        'light-blue': '#96E0DE',
+        blue: '#76CDCE',
+        'light-green': '#D2EA88',
+        green: '#B5D861',
+        'dark-green': '#99B752',
+        'forest-green': '#4B6A22',
+        'grass-green': '#8ac43d'
+      },
+      margin: {
+        '80': '20rem'
+      },
+      width: {
+        '84': '21rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
